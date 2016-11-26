0ed02c7acc1b55c8471c4d79c71732b2ef4384ce
Maurice Henke <speeduplp@gmail.com>
2016-11-26 11:35:13 +0100

    Add files via upload

--- osu.ppy.sh/inc/pages/Leaderboard.php
+++ osu.ppy.sh/inc/pages/Leaderboard.php
@@ -4,7 +4,7 @@ class Leaderboard {
 	const PageID = 13;
 	const URL = 'leaderboard';
 	const Title = 'Bannedcho - Leaderboard';
-	const LoggedIn = true;
+	//const LoggedIn = true;
 
 	public function P() {
 		// Leaderboard names (to bold the selected mode)
@@ -23,13 +23,14 @@ class Leaderboard {
 		// Bold the selected mode
 		$modesText[$m] = '<b>'.$modesText[$m].'</b>';
 		// Header stuff
-		echo '<blockquote><p>Plz enjoy game.</p><footer>rrtyui</footer></blockquote>';
+		echo '<blockquote><p><font size="4"><i>"Ouchi ouchi ouch.<br>Ouch again."</font></i></p><footer>Gideon Strohmeyer</footer></blockquote>';
 		echo '<a href="index.php?p=13&m=0">'.$modesText[0].'</a> | <a href="index.php?p=13&m=1">'.$modesText[1].'</a> | <a href="index.php?p=13&m=2">'.$modesText[2].'</a> | <a href="index.php?p=13&m=3">'.$modesText[3].'</a>';
 		// Leaderboard
 		echo '<table class="table table-striped table-hover">
 		<thead>
 		<tr>
 		<th>Rank</th>
+		<th>League</th>
 		<th>Player</th>
 		<th>Accuracy</th>
 		<th>Playcount</th>
@@ -37,7 +38,7 @@ class Leaderboard {
 		</tr>
 		</thead>';
 		echo '<tbody>';
-		// Get all user data and order them by score
+		// Get all user data and order them by pp
 		$tb = 'leaderboard_'.getPlaymodeText($m);
 		$leaderboard = $GLOBALS['db']->fetchAll("SELECT users_stats.*, $tb.* FROM users_stats INNER JOIN $tb ON users_stats.id=$tb.user ORDER BY $tb.position;");
 		// Set rank to 0
@@ -67,7 +68,9 @@ class Leaderboard {
 				} else {
 					$country = 'xx';
 				}
-				echo '<td><img src="./images/flags/'.$country.'.png"/>	<a href="index.php?u='.$lbUser['id'].'&m='.$m.'">'.$lbUser['username'].'</a></td>
+				$division = '<img src="http://sk2.op.gg/images/medals/'.getLeague($lbUser['id']).'.png" height="28"></img>';
+				echo '<td>'.$division.strtoupper(substr(getLeague($lbUser['id']),0,-2)).'</td>
+				<td><img src="./images/flags/'.$country.'.png"/>	<a href="index.php?u='.$lbUser['id'].'&m='.$m.'">'.$lbUser['username'].'</a></td>				
 				<td>'.(is_numeric($lbUser['avg_accuracy_'.$modeForDB]) ? accuracy($lbUser['avg_accuracy_'.$modeForDB]) : '0.00').'%</td>
 				<td>'.number_format($lbUser['playcount_'.$modeForDB]).'<i> (lvl.'.$lbUser['level_'.$modeForDB].')</i></td>
 				<td>'.number_format($lbUser['pp_'.$modeForDB]).' pp</td>
@@ -102,38 +105,40 @@ class Leaderboard {
 				continue;
 			}
 			foreach ($modes as $mode) {
-				$data[$mode][] = ['user' => $user['id'], 'score' => $user['pp_'.$mode]];
+				$data[$mode][] = ['user' => $user['id'], 'pp' => $user['pp_'.$mode]];
 			}
 		}
 		// We're doing the sorting for every mode.
-		// Also 'score' is the pp of the user
+		// Also 'pp' is the pp of the user
 		foreach ($modes as $mode) {
 			// Do the sorting
 			usort($data[$mode], function ($a, $b) {
-				if ($a['score'] == $b['score']) {
+				if ($a['pp'] == $b['pp']) {
 					return 0;
 				}
 				// We're doing ? 1 : -1 because we're doing in descending order.
-				return ($a['score'] < $b['score']) ? 1 : -1;
+				return ($a['pp'] < $b['pp']) ? 1 : -1;
 			});
 			// Remove all data from the table
 			$GLOBALS['db']->execute("TRUNCATE TABLE leaderboard_$mode;");
 			// And insert each user.
 			foreach ($data[$mode] as $key => $val) {
-				$GLOBALS['db']->execute("INSERT INTO leaderboard_$mode (position, user, v) VALUES (?, ?, ?)", [$key + 1, $val['user'], $val['score']]);
+				if($val['user'] != 999) {
+					$GLOBALS['db']->execute("INSERT INTO leaderboard_$mode (position, user, v) VALUES (?, ?, ?)", [$key + 1, $val['user'], $val['pp']]);
+				}
 			}
 		}
 	}
 
-	public static function Update($userID, $newScore, $mode) {
+	public static function Update($userID, $newpp, $mode) {
 		// Who are we?
 		$us = $GLOBALS['db']->fetch("SELECT * FROM leaderboard_$mode WHERE user=?", [$userID]);
 		$newplayer = false;
 		if (!$us) {
 			$newplayer = true;
 		}
-		// Find player who is right below our score
-		$target = $GLOBALS['db']->fetch("SELECT * FROM leaderboard_$mode WHERE v <= ? ORDER BY position ASC LIMIT 1", [$newScore]);
+		// Find player who is right below our pp
+		$target = $GLOBALS['db']->fetch("SELECT * FROM leaderboard_$mode WHERE v <= ? ORDER BY position ASC LIMIT 1", [$newpp]);
 		$plus = 0;
 		if (!$target) {
 			// Wow, this user completely sucks at this game.
@@ -157,6 +162,8 @@ class Leaderboard {
 			$GLOBALS['db']->execute("UPDATE leaderboard_$mode SET position = position + 1 WHERE position < ? AND position >= ? ORDER BY position DESC", [$us['position'], $newT]);
 		}
 		// Finally, insert the user back.
-		$GLOBALS['db']->execute("INSERT INTO leaderboard_$mode (position, user, v) VALUES (?, ?, ?);", [$newT, $userID, $newScore]);
+		if($val['user'] != 999) {
+			$GLOBALS['db']->execute("INSERT INTO leaderboard_$mode (position, user, v) VALUES (?, ?, ?);", [$newT, $userID, $newpp]);
+		}
 	}
 }
